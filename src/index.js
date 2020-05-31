391d6aa2d736b814cae72ed4ec3298b468366eb8
MisterC1ean <robert.keller44@icloud.com>
2020-05-31 11:53:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -18,5 +18,29 @@ function render() {
   }
 };
 
+// Write a function called createStore that takes in a reducer function as an argument.
+// The createStore function should return an object with two methods - getState, dispatch.
+// getState should return the current state.
+function createStore(reducer) {
+  return(
+    {
+      getState(){
+        return(state)
+      },
+
+      dispatch(action){
+        reducer(action)
+        render()
+      }
+    }
+  )
+}
+
+// dispatch should take in an action, update the state using the reducer, and call the render function.
+// Once you've written createStore, to everything working, you'll need two more things at the end of index.js:
+
+// Notice that in the provided render function, a variable named store is called. Define a variable named store and assign it to whatever is returned by the createStore function you've written when it is passed the candyReducer function.
+// // Initialize your state using a dispatch call to the reducer.
+
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
